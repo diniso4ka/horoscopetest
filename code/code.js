688498dea80a0bcdf7cedb987d6a755b8225f3b8
diniso4ka <100791507+diniso4ka@users.noreply.github.com>
2022-04-24 00:51:05 +0300

    Add files via upload

--- code/code.js
+++ code/code.js
@@ -6,6 +6,7 @@ let sup = document.getElementById("support")
 let message = [
    "Уебан, если не извинишься, тебя завтра фура собъет, поэтому сейчас открываешь мою фотографию и стоя на коленях целуешь её 3 три раза произнося \"Дениска не простит, боженька простит\"."
 ]
+let messageText = document.getElementById("thankstext")
 let messageAlert = document.getElementById("text")
 let confContainer = document.getElementById("confirm-container")
 let confButton1 = document.getElementById("confirmButton1")
@@ -22,8 +23,12 @@ getMessage = () => {
    if (num === "") {
       messageAlert.innerHTML = `Ты блять, уебанище, если не извинишься, тебя завтра фура собъет, поэтому сейчас же открываешь мою фотографию и стоя на коленях целуешь её 3 три раза произнося \"Дениска не простит, боженька простит\".`
    } else if (num === "денис" || num === "денчик") {
-      messageAlert.innerHTML = `Денис Анатольвеич, святой вы человек, дай бог вам здоровья!!!>`
-   } else
+      messageAlert.innerHTML = `Денис Анатольвеич, святой вы человек, дай бог вам здоровья!!!`
+   }
+   else if (num === "владимир" || num === "вова") {
+      messageAlert.innerHTML = `Интересный факт, за 10 000 часов войны он уничтожил 0,76 украинский фур.`
+   }
+   else
       messageAlert.innerHTML = `${name}, если не извинишься, тебя завтра фура собъет, поэтому сейчас же открываешь мою фотографию и стоя на коленях целуешь её 3 три раза произнося \"Дениска не простит, боженька простит\"."`
 };
 
@@ -33,11 +38,22 @@ getThanks = () => {
    sup.className = "supVisible"
 }
 
+swapText = () => {
+   messageText.innerHTML = "Поддерживаете ли вы Алькаиду?"
+}
+
+returnText = () => {
+   messageText.innerHTML = "Готовы ли продолжить опрос?"
+}
+
 
 
 
 
 
 buttonClick.addEventListener("click", getMessage)
+confButton1.addEventListener("mouseover", swapText)
+confButton1.addEventListener("mouseout", returnText)
 confButton1.addEventListener("click", getThanks)
-confButton2.addEventListener("click", getThanks)
+confButton2.addEventListener("click", getThankss)
+
